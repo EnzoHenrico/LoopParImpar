6eaf62321dfa28ca3abd1508388801f91ef66563
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-19 00:37:30 -0300

    Add project files.

---
+++ LoopParImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿// Faça um programa que leia um número inicial e um número final, mostrando entre eles quais são os valores pares e impares.
+int init = 0, final = 0; 
+
+Console.WriteLine("Amostragem de números pares e impares.");
+
+Console.Write("Fale o valor inicial: ");
+init = int.Parse(Console.ReadLine());
+
+Console.Write("Fale o valor final: ");
+final = int.Parse(Console.ReadLine());
+
+for  (int i = init + 1; i < final; i++)
+{
+    if ((i % 2 ) == 0)
+    {
+        Console.WriteLine($"O númeor {i} é par.");
+    }
+    else 
+    {
+        Console.WriteLine($"O númeor {i} é impar.");
+    }
+}
